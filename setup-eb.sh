582fe38fd34823a88459972cf8eb5e40cf806612
john <john_lin@mypay.tw>
2016-07-23 17:06:40 +0800

    Update setup-eb.sh

--- setup-eb.sh
+++ setup-eb.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-# set -x
-# set -e
-# if [ ! -d "/home/ubuntu/.aws" ]; then
-#     mkdir /home/ubuntu/.aws
-# fi
-# if [ ! -f "/home/ubuntu/.aws/config" ]; then
-#     touch chmod 600 /home/ubuntu/.aws/config    
-# fi
-# echo "[profile eb-cli]" > /home/ubuntu/.aws/config
-# echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
-# echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
+set -x
+set -e
+if [ ! -d "/home/ubuntu/.aws" ]; then
+    mkdir /home/ubuntu/.aws
+fi
+if [ ! -f "/home/ubuntu/.aws/config" ]; then
+    touch chmod 600 /home/ubuntu/.aws/config    
+fi
+echo "[profile eb-cli]" > /home/ubuntu/.aws/config
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
 
-# value=`cat /home/ubuntu/.aws/config`
-# echo "$value"
+value=`cat /home/ubuntu/.aws/config`
+echo "$value"
 
-mv .elasticbeanstalk/config.global.yml .elasticbeanstalk/config.yml
+#mv .elasticbeanstalk/config.global.yml .elasticbeanstalk/config.yml
 
  
 
